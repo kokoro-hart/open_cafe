f8f9e923fb978316d11da73ae8d8dab06954f7c0
kokoro <cocosyu.11261126@gmail.com>
2021-11-19 20:39:05 +0900

    Add: 遅延読み込み intersection observerを追加

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -8,6 +8,33 @@
 //slickを使うとき
 // import '@modules/slick-carousel';
 
+//画像遅延読み込み
+const lazy = document.querySelectorAll('.lazyload');
+const lazyObserver = new IntersectionObserver(inViewport, {
+  rootMargin: '50%',
+  threshold: [0]
+});
+
+Array.from(lazy).forEach(element => {
+  lazyObserver.observe(element);
+});
+
+function inViewport(entries, observer) {
+  entries.forEach(entry => {
+
+    if (entry.intersectionRatio > 0) {
+      const imgElement = entry.target;
+      imgElement.src = imgElement.dataset.src;
+
+      imgElement.addEventListener('load', () => {
+        imgElement.classList.add('is-lazyloaded');
+      });
+
+      lazyObserver.unobserve(entry.target);
+    }
+  });
+}
+
 //ビューポート幅360px以下はリサイズして表示
 !(function () {
   const viewport = document.querySelector('meta[name="viewport"]');
