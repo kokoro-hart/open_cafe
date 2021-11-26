d65a505b34b175ce0b4ac5a5200b151456f23ec6
kokoro <cocosyu.11261126@gmail.com>
2021-11-26 20:53:54 +0900

    Add: page-concept.phpを追加

---
+++ wp/page-concept.php
@@ -0,0 +1,163 @@
+<?php get_header(); ?>
+
+<main>
+  <section class="p-lower-mv">
+    <picture class="p-lower-mv__picture">
+      <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/img-mv-concept.webp" type="image/webp" />
+      <img src="<?php echo get_template_directory_uri(); ?>/img/common/img-mv-concept.jpeg" class="p-lower-mv__img" decoding="async" width="1280" height="320" alt="コンセプトメインビジュアル">
+    </picture>
+    <h1 class="p-lower-mv__heading">
+      <p class="p-lower-mv__head">CONCEPT</p>
+      <p class="p-lower-mv__lead">当店のこだわり</p>
+    </h1>
+  </section>
+  <div class="c-breadcrumb u-mt5">
+    <div class="l-inner">
+      <ul class="c-breadcrumb__list" itemscope itemtype="https://schema.org/BreadcrumbList">
+        <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+          <a class="c-breadcrumb__link" itemprop="item" href="/"><span itemprop="name">HOME</span></a>
+          <meta itemprop="position" content="1" />
+        </li>
+        <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+          <a class="c-breadcrumb__link" itemprop="item" href="/concept/"><span itemprop="name">当店のこだわり</span></a>
+          <meta itemprop="position" content="2" />
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="l-lower-concept">
+    <section class="p-concept l-lower-concept__detail">
+      <div class="p-concept__inner">
+        <picture class="p-concept__picture">
+          <source class="p-concept__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/img-concept02.webp" type="image/webp" />
+          <img src="<?php echo get_template_directory_uri(); ?>/img/common/img-concept02.png" class="p-concept__img" width="640" height="730" decoding="async" alt="コンセプトイメージ">
+        </picture>
+        <div class="p-concept__contents">
+          <h2 class="p-concept__title">
+            美味しいコーヒーと食事で、<br>最高のひとときを。
+          </h2>
+          <p class="p-concept__text">
+            ダミー_国内外から賞賛を
+            <br>
+            受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。
+            <br>
+            ダミー_国内外から賞賛を受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。
+            <br>
+            <br>
+            ダミー_国内外から賞賛を受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="p-concept p-concept--reverse l-lower-concept__detail">
+      <div class="p-concept__inner">
+        <picture class="p-concept__picture">
+          <source class="p-concept__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/img-concept03.webp" type="image/webp" />
+          <img data-src="<?php echo get_template_directory_uri(); ?>/img/common/img-concept03.png" class="p-concept__img lazyload" width="640" height="537" alt="コンセプトイメージ">
+        </picture>
+        <div class="p-concept__contents">
+          <h2 class="p-concept__title">
+            本場イタリアで培った<br>自慢のパスタ
+          </h2>
+          <p class="p-concept__text">
+            ダミー_国内外から賞賛を
+            <br>
+            受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。
+            <br>
+            ダミー_国内外から賞賛を受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。
+            <br>
+            <br>
+            ダミー_国内外から賞賛を受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="p-concept l-lower-concept__detail">
+      <div class="p-concept__inner">
+        <picture class="p-concept__picture">
+          <source class="p-concept__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/img-concept04.webp" type="image/webp" />
+          <img data-src="<?php echo get_template_directory_uri(); ?>/img/common/img-concept04.jpeg" class="p-concept__img lazyload" width="640" height="400" decoding="async" alt="コンセプトイメージ">
+        </picture>
+        <div class="p-concept__contents">
+          <h2 class="p-concept__title">
+            ほどよい甘さの<br>自家製こだわりクロワッサン
+          </h2>
+          <p class="p-concept__text">
+            ダミー_国内外から賞賛を受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。<br>
+            ダミー_国内外から賞賛を受けた選りすぐりのデザイナーが集結し、ガーデニングの設計・建築から料理まで、あらゆる空間が誕生。
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+  <section class="l-access">
+    <div class="p-access">
+      <h2 class="c-section-title">
+        <span class="c-section-title__en">ACCESS</span>
+        <span class="c-section-title__ja">アクセス</span>
+      </h2>
+      <div class="p-access__inner">
+        <div class="p-map">
+          <iframe data-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.0372058864655!2d139.5780239156885!3d35.70070203018986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018ee39b555205f%3A0xabb26a0a2fbda595!2z44CSMTgwLTAwMDMg5p2x5Lqs6YO95q2m6JS16YeO5biC5ZCJ56Wl5a-65Y2X55S677yR5LiB55uu!5e0!3m2!1sja!2sjp!4v1637065433936!5m2!1sja!2sjp" width="688" height="387" style="border:0;" allowfullscreen="" class="lazyload">
+          </iframe>
+        </div>
+        <address class="p-info">
+          <dl class="p-info__dl">
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                住所
+              </dt>
+              <dd class="p-info__dd">
+                〒000-0000<br>東京都武蔵野市吉祥寺南町一丁目
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                TEL
+              </dt>
+              <dd class="p-info__dd">
+                <a href="tel:0123-456-789">0123-456-789</a>
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                Mail
+              </dt>
+              <dd class="p-info__dd">
+                <a href="mailto:example@mail.com">example@mail.com</a>
+              </dd>
+            </div>
+          </dl>
+          <dl class="p-info__dl">
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                営業時間
+              </dt>
+              <dd class="p-info__dd">
+                7:00〜21:00<br>※ラストオーダー 20:30
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                定休日
+              </dt>
+              <dd class="p-info__dd">
+                水曜日
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                座席
+              </dt>
+              <dd class="p-info__dd">
+                テーブル20席 ／ カウンター席6席
+              </dd>
+            </div>
+          </dl>
+        </address>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
