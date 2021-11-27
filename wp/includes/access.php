8d91baee3caa6f69c6852e633569cdfcd239ac6d
kokoro <cocosyu.11261126@gmail.com>
2021-11-27 17:20:47 +0900

    Add: アクセスをパーツ化

---
+++ wp/includes/access.php
@@ -0,0 +1,68 @@
+  <section class="l-access">
+    <div class="p-access">
+      <h2 class="c-section-title">
+        <span class="c-section-title__en">ACCESS</span>
+        <span class="c-section-title__ja">アクセス</span>
+      </h2>
+      <div class="p-access__inner">
+        <div class="p-map">
+          <iframe data-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.0372058864655!2d139.5780239156885!3d35.70070203018986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018ee39b555205f%3A0xabb26a0a2fbda595!2z44CSMTgwLTAwMDMg5p2x5Lqs6YO95q2m6JS16YeO5biC5ZCJ56Wl5a-65Y2X55S677yR5LiB55uu!5e0!3m2!1sja!2sjp!4v1637065433936!5m2!1sja!2sjp" width="688" height="387" style="border:0;" allowfullscreen="" class="lazyload">
+          </iframe>
+        </div>
+        <address class="p-info">
+          <dl class="p-info__dl">
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                住所
+              </dt>
+              <dd class="p-info__dd">
+                〒000-0000<br>東京都武蔵野市吉祥寺南町一丁目
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                TEL
+              </dt>
+              <dd class="p-info__dd">
+                <a href="tel:0123-456-789">0123-456-789</a>
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                Mail
+              </dt>
+              <dd class="p-info__dd">
+                <a href="mailto:example@mail.com">example@mail.com</a>
+              </dd>
+            </div>
+          </dl>
+          <dl class="p-info__dl">
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                営業時間
+              </dt>
+              <dd class="p-info__dd">
+                7:00〜21:00<br>※ラストオーダー 20:30
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                定休日
+              </dt>
+              <dd class="p-info__dd">
+                水曜日
+              </dd>
+            </div>
+            <div class="p-info__row">
+              <dt class="p-info__dt">
+                座席
+              </dt>
+              <dd class="p-info__dd">
+                テーブル20席 ／ カウンター席6席
+              </dd>
+            </div>
+          </dl>
+        </address>
+      </div>
+    </div>
+  </section>
