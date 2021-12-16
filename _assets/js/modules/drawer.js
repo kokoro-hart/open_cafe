f0a88a6cf11abc6eeaaa978e4e1c6184c68b36f2
kokoro <cocosyu.11261126@gmail.com>
2021-12-16 23:29:07 +0900

    Add: mvMenuがビューポート内にあるときはドロワーボタンは非表示

--- _assets/js/modules/drawer.js
+++ _assets/js/modules/drawer.js
@@ -1,9 +1,9 @@
 
-const drawerButton = document.getElementById('js-drawer-button');
-const drawerBg = document.getElementById('js-drawer-bg');
-const body = document.body;
-const drawerMenu = document.getElementById('js-global-nav');
+const drawerButton = document.getElementById('js-drawer-button'); //ドロワーボタン
+const drawerBg = document.getElementById('js-drawer-bg'); //ドロワー背景
+const drawerMenu = document.getElementById('js-global-nav'); //ドロワーメニュー
 
+//ドロワーを閉じる関数
 function closeDrawerMenu() {
   drawerButton.setAttribute('aria-expanded', 'false');
   drawerButton.classList.remove('is-active');
@@ -11,9 +11,9 @@ function closeDrawerMenu() {
   drawerMenu.setAttribute('area-hidden', 'true');
   drawerMenu.classList.remove('is-active');
 }
-
+//開閉イベント
 drawerButton.addEventListener('click', function () {
-  body.classList.toggle('is-drawerActive');
+  document.body.classList.toggle('is-drawerActive');
   if (drawerButton.getAttribute('aria-expanded') == 'false') {
     this.setAttribute('aria-expanded', 'true');
     this.classList.add('is-active');
@@ -24,8 +24,25 @@ drawerButton.addEventListener('click', function () {
     closeDrawerMenu();
   };
 });
-
+//背景クリック時にもドロワーを閉じる
 drawerBg.addEventListener('click', function () {
-  body.classList.toggle('is-drawerActive');
+  document.body.classList.toggle('is-drawerActive');
   closeDrawerMenu();
 });
+
+const mvMenu = document.getElementById('js-mv-menu'); //メインビジュアルメニュー
+//mvMenuがビューポート内にあるときはドロワーボタンは非表示
+const drawerObserver = new IntersectionObserver((entries) => {
+  for (const e of entries) {
+    if (e.isIntersecting) {
+      drawerButton.style.opacity = 0;
+      drawerButton.style.visibility = 'hidden';
+    } else {
+      drawerButton.style.opacity = 1;
+      drawerButton.style.visibility = 'visible';
+    }
+  }
+});
+if (mvMenu !== null) {
+  drawerObserver.observe(mvMenu);// 監視対象
+}
