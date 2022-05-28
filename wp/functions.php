82f5c7388cea6a5c764073ece26ddceedb32af0a
kokoro <cocosyu.11261126@gmail.com>
2022-05-28 14:55:28 +0900

    Fix: ogpを functions.phpで管理

--- wp/functions.php
+++ wp/functions.php
@@ -1,6 +1,8 @@
 <?php
 
-// セットアップ
+/**********************
+初期設定
+*********************/
 function my_setup()
 {
   add_theme_support('post-thumbnails'); // アイキャッチ画像を有効化
@@ -19,13 +21,6 @@ function my_setup()
 }
 add_action('after_setup_theme', 'my_setup');
 
-//タイトルの区切り
-function change_title_separator( $sep ){
-  $sep = ' | ';
-  return $sep;
-}
-add_filter( 'document_title_separator', 'change_title_separator' );
-
 //css jsの読み込み
 function my_script_init()
 {
@@ -38,9 +33,109 @@ function my_script_init()
   //my
   wp_enqueue_style('my', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
   wp_enqueue_script('my', get_template_directory_uri() . '/js/bundle.js', array(), '1.0.0', true);
-
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
+
+
+/**********************
+OGP設定
+*********************/
+function my_meta_ogp()
+{
+  if (is_front_page() || is_home() || is_singular() || is_archive()) {
+
+    /*初期設定*/
+
+    // 画像 （アイキャッチ画像が無い時に使用する代替画像URL）
+    $ogp_image = ''.esc_url(get_template_directory_uri()).'/img/common/img-mv01_pc.jpeg';
+    // Twitterのアカウント名 (@xxx)
+    $twitter_site = '@dummy_open_cafe';
+    // Twitterカードの種類（summary_large_image または summary を指定）
+    $twitter_card = 'summary_large_image';
+    // Facebook APP ID
+    $facebook_app_id = '';
+
+    /*初期設定 ここまで*/
+
+    global $post;
+    $ogp_title = '';
+    $ogp_description = '';
+    $ogp_url = '';
+    $html = '';
+
+    if (is_front_page()) {
+      // トップページ
+      $ogp_title = get_bloginfo('name');
+      $ogp_description = get_bloginfo('description');
+      $ogp_url = home_url();
+    } elseif (is_tax() || is_category())  {
+      // カテゴリページ
+      $ogp_title = single_cat_title( '', false ) . ' | ' . get_bloginfo('name');
+      $ogp_description = get_bloginfo('description');
+      $ogp_url =get_pagenum_link(get_query_var('paged'));
+    } elseif (is_page()) {
+      // 固定ページ
+      $ogp_title = get_the_title() . ' | ' . get_bloginfo('name');
+      $ogp_description = get_bloginfo('description');
+      $ogp_url = get_pagenum_link(get_query_var('paged'));
+      wp_reset_postdata();
+    } elseif (is_archive('news'))  {
+      // メニューページ
+      $ogp_title = 'メニュー | ' . get_bloginfo('name');
+      $ogp_description = get_bloginfo('description');
+      $ogp_url =get_pagenum_link(get_query_var('paged'));
+    } elseif (is_home()) {
+      // お知らせページ
+      $ogp_title = 'お知らせ | ' . get_bloginfo('name');
+      $ogp_description = get_bloginfo('description');
+      $ogp_url =get_pagenum_link(get_query_var('paged'));
+    } elseif (is_singular()) {
+      // 記事＆固定ページ
+      setup_postdata($post);
+      $ogp_title = $post->post_title . ' | ' . get_bloginfo('name');
+      $ogp_description = mb_substr(get_the_excerpt(), 0, 100);
+      $ogp_url = get_permalink();
+      wp_reset_postdata();
+    }
+
+    // og:type
+    $ogp_type = (is_front_page()) ? 'website' : 'article';
+
+    // og:image
+    if (is_singular() && has_post_thumbnail()) {
+      $ps_thumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
+      $ogp_image = $ps_thumb[0];
+    }
+
+    // 出力するOGPタグをまとめる
+    $html = "\n";
+    $html .= '<meta property="og:title" content="' . esc_attr($ogp_title) . '">' . "\n";
+    $html .= '<meta property="og:description" content="' . esc_attr($ogp_description) . '">' . "\n";
+    $html .= '<meta property="og:type" content="' . $ogp_type . '">' . "\n";
+    $html .= '<meta property="og:url" content="' . esc_url($ogp_url) . '">' . "\n";
+    $html .= '<meta property="og:image" content="' . esc_url($ogp_image) . '">' . "\n";
+    $html .= '<meta property="og:site_name" content="' . esc_attr(get_bloginfo('name')) . '">' . "\n";
+    $html .= '<meta name="twitter:card" content="' . $twitter_card . '">' . "\n";
+    $html .= '<meta name="twitter:site" content="' . $twitter_site . '">' . "\n";
+    $html .= '<meta property="og:locale" content="ja_JP">' . "\n";
+
+    if ($facebook_app_id != "") {
+      $html .= '<meta property="fb:app_id" content="' . $facebook_app_id . '">' . "\n";
+    }
+
+    echo $html;
+  }
+}
+
+/**********************
+フィルターフック
+*********************/
+//タイトルの区切り
+function change_title_separator( $sep ){
+  $sep = ' | ';
+  return $sep;
+}
+add_filter( 'document_title_separator', 'change_title_separator' );
 
 //フォーム設置ページのみContactForm7のcss、jsを読み込み
 add_action( 'wp', function() 
@@ -74,6 +169,10 @@ function scriptLoader($script, $handle, $src) {
 }
 add_filter('script_loader_tag', 'scriptLoader', 10, 5);
 
+
+/**********************
+カスタム投稿設定
+*********************/
 add_action('init', function() 
 {
   //メニュー
--- wp/header.php
+++ wp/header.php
@@ -6,14 +6,8 @@
   <meta name="viewport" content="width=device-width">
   <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/common/apple-touch-icon.png">
   <link rel="icon" href="<? echo get_template_directory_uri(); ?>/img/common/favicon.ico" id="favicon">
-  <meta name="description" content="パスタとコーヒーがとってもおいしい、ほっと落ち着くのんびり空間。">
-  <meta property="og:title" content="<?php the_title(); ?> | Open Cafe - dish &amp; coffee - ">
-  <meta property="og:description" content="パスタとコーヒーがとってもおいしい、ほっと落ち着くのんびり空間。">
-  <meta property="og:url" content="<?php echo esc_url(home_url('/')); ?>">
-  <meta property="og:type" content="website ">
-  <meta property="og:local" content="ja_JP">
-  <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/common/img-mv01_pc.jpeg">
-  <meta property="og:site_name" content="Open Cafe - dish &amp; coffee -">
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <?php my_meta_ogp(); ?>
 
   <?php wp_head(); ?>
 </head>
@@ -21,6 +15,7 @@
   <div id="wrapper">
     <header class="l-header p-header">
       <div class="p-header__inner">
+        <?php if(is_front_page()) : ?>
         <p class="p-header__logo u-hidden-xl-up">
           <a href="<?php echo esc_url(home_url('/')); ?>" class="p-header__logo-link">
             <svg class="c-svg p-header__logo-icon" width="120" height="64">
@@ -28,19 +23,20 @@
             </svg>
           </a>
         </p>
+        <?php endif; ?>
       </div>
       <button id="js-drawer-button" class="p-header__button c-button-drawer" aria-controls="js-drawer-menu" aria-expanded="false">
         <span class="c-button-drawer__line"></span>
       </button>
       <div id="js-drawer-bg" class="p-header__drawer-bg"></div>
       <div class="p-header-menu" id="js-drawer-menu" area-hidden="true">
-        <p class="p-header__logo">
-          <a href="/" class="p-header__logo-link">
-            <svg class="c-svg p-header__logo-icon" width="120" height="64">
-              <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#logo02" />
-            </svg>
-          </a>
-        </p>
+          <p class="p-header__logo">
+            <a href="<?php echo esc_url(home_url('/')); ?>" class="p-header__logo-link">
+              <svg class="c-svg p-header__logo-icon" width="120" height="64">
+                <use xlink:href="<?php echo get_template_directory_uri(); ?>/img/svg/sprite.min.svg#logo02" />
+              </svg>
+            </a>
+          </p>
         <nav class="p-header-menu__nav p-global-nav">
           <ul class="p-global-nav__list">
             <li class="p-global-nav__item">
